89b0c3e32e1c975d3f27142c2392e0d40094321b
Victoria Skeggs <victoria.skeggs@gmail.com>
2017-05-16 21:27:39 +1200

    Update EndPane.java

--- EndPane.java
+++ EndPane.java
@@ -14,37 +14,51 @@ public class EndPane extends JPanel {
 	
 	private FlowLayout _layout;
 	private Border _border;
+	private int _flowNum;
+	private Color _borderCol;
+	private int _borderSize;
+	private Color _backgroundCol;
+	private Color _fontCol;
+	private int _fontSize;
+	private String[] _line = new String[2];
 
 	public void loseMinesweeper() {
-		_layout = new FlowLayout(1);
-		setLayout(_layout);
-		_border = BorderFactory.createLineBorder(Color.BLACK, 100);
-		setBorder(_border);
-		setBackground(Color.BLACK);
+		_flowNum = 1;
+		_borderCol = Color.BLACK;
+		_borderSize = 100;
+		_backgroundCol = Color.BLACK;
+		_fontCol = Color.RED;
+		_fontSize = 40;
+		_line[0] = "YOU LOSE! ";
+		_line[1] = "";
 		
-		JLabel message = new JLabel("YOU LOSE!");
-		message.setFont(new Font("Arial", Font.PLAIN, 40));
-		message.setForeground(Color.RED);
-		add(message);
+		endMinesweeper();
 	}
 	
 	public void winMinesweeper() {
-		_layout = new FlowLayout(2);
-		setLayout(_layout);
-		_border = BorderFactory.createLineBorder(Color.YELLOW, 70);
-		setBorder(_border);
-		setBackground(Color.GREEN);
+		_flowNum = 2;
+		_borderCol = Color.YELLOW;
+		_borderSize = 70;
+		_backgroundCol = Color.GREEN;
+		_fontCol = Color.BLUE;
+		_fontSize = 35;
+		_line[0] = "CONGRATULATIONS!"; 
+		_line[1] = "YOU WIN!         ";
 		
-		for (int iterations = 1; iterations <= 2; iterations++) {
-			String line;
-			if (iterations == 1) {
-				line = "CONGRATULATIONS!";
-			} else {
-				line = "YOU WIN!         ";
-			}
-			JLabel message = new JLabel(line);
-			message.setFont(new Font("Arial", Font.PLAIN, 35));
-			message.setForeground(Color.BLUE);
+		endMinesweeper();
+	}
+	
+	private void endMinesweeper() {
+		_layout = new FlowLayout(_flowNum);
+		setLayout(_layout);
+		_border = BorderFactory.createLineBorder(_borderCol, _borderSize);
+		setBorder(_border);
+		setBackground(_backgroundCol);
+		
+		for (int index = 0; index <= 1; index++) {
+			JLabel message = new JLabel(_line[index]);
+			message.setFont(new Font("Arial", Font.PLAIN, _fontSize));
+			message.setForeground(_fontCol);
 			add(message);
 		}
 	}
